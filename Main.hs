79cabac8c66e5f9189c71255a33bef02ae1a4208
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2020-07-08 19:53:15 -0300

    cores

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+
 import qualified Data.Matrix as Matrix
 import qualified Rainbow
 import Data.Function ((&))
@@ -15,9 +16,8 @@ data Piece = Pawn
 data ChessPiece = ChessPiece {
     player :: Player,
     piece :: Piece
-} | None deriving(Show, Eq)
+} | Space deriving(Show, Eq)
 
-pieceStr :: Piece -> String
 pieceStr p
     | p == Pawn   = "p"
     | p == Rook   = "R"
@@ -27,27 +27,28 @@ pieceStr p
     | p == King   = "K"
 
 
-chunkPiece :: ChessPiece -> Rainbow.Chunk
-chunkPiece None                     = "."
-chunkPiece (ChessPiece White piece) = Rainbow.fromText (pieceStr piece) <> Rainbow.fore Rainbow.blue
-chunkPiece (ChessPiece Black piece) = Rainbow.fromText (pieceStr piece) <> Rainbow.fore Rainbow.red
+chunkPiece Space                    = "."
+chunkPiece (ChessPiece White piece) = pieceStr piece & Rainbow.fore Rainbow.magenta--(Rainbow.blue <> Rainbow.only256 Rainbow.blue)
+chunkPiece (ChessPiece Black piece) = pieceStr piece & Rainbow.fore Rainbow.green--(Rainbow.red <> Rainbow.only256 Rainbow.red)
 
+elemToChunk (r, c) e = if c == 8 then chunk <> "\n" else chunk
+        where 
+            chunk = chunkPiece e
 
+getChessboardStr = Matrix.toList . Matrix.mapPos elemToChunk
 
 initialPosition :: (Int, Int) -> ChessPiece
 initialPosition (x, y) 
     | x == 2 || x == 7 = createPiece Pawn
-    | x /= 1 && x /= 8 = None
+    | x /= 1 && x /= 8 = Space
     | y == 1 || y == 8 = createPiece Rook
     | y == 2 || y == 7 = createPiece Knight
     | y == 3 || y == 6 = createPiece Bishop
     | y == 4           = createPiece Queen
     | y == 5           = createPiece King
-    | otherwise        = None
+    | otherwise        = Space
     where 
         player = if x <= 2 then White else Black 
         createPiece p = ChessPiece player p
 
-main = do
-    print $ Matrix.matrix 8 8 initialPosition
-    Rainbow.putChunkLn $ chunkPiece (ChessPiece White Knight)
+main = Rainbow.putChunksLn $ getChessboardStr (Matrix.matrix 8 8 initialPosition)
